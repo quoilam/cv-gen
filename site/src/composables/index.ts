85454aa95cf0f091b05d814092c3d1a288a7e9e0
imag-er <2274265784@qq.com>
2026-05-23 05:55:28 +0800

    refactor: remove useAsset export from composables index

--- site/src/composables/index.ts
+++ site/src/composables/index.ts
@@ -2,7 +2,6 @@ export { useDataStore } from "./stores/data";
 export { useStyleStore } from "./stores/style";
 export { useMonaco } from "./monaco";
 export { useConstant } from "./constant";
-export { useAsset } from "./asset";
 export { useAI } from "./ai";
 export { useIcon } from "./icon";
 export { useGit } from "./git";
