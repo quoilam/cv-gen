2a136f8faa0d13305670f79d04c55bd8a4a81892
imag-er <2274265784@qq.com>
2026-08-23 23:25:04 +0800

    fix(pwa): 修复 manifest 与 service worker 路径未考虑 baseURL

--- site/configs/pwa.ts
+++ site/configs/pwa.ts
@@ -1,6 +1,6 @@
 import type { ModuleOptions } from "@vite-pwa/nuxt";
 
-const scope = "/";
+const scope = "/cv-gen/";
 
 export const pwa: ModuleOptions = {
   registerType: "autoUpdate",
@@ -13,17 +13,17 @@ export const pwa: ModuleOptions = {
     short_name: "CvGen",
     icons: [
       {
-        src: "/pwa-192x192.png",
+        src: "pwa-192x192.png",
         sizes: "192x192",
         type: "image/png"
       },
       {
-        src: "/pwa-512x512.png",
+        src: "pwa-512x512.png",
         sizes: "512x512",
         type: "image/png"
       },
       {
-        src: "/favicon.svg",
+        src: "favicon.svg",
         sizes: "512x512",
         type: "image/svg",
         purpose: "any maskable"
